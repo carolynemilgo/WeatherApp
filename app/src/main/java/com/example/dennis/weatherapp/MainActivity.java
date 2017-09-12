6f5a08121ad35cd64cc2ae09281bdee02b533f46
dennismuturia <dmuturia2922@gmail.com>
2017-09-12 16:59:24 +0300

    Added a butterknife
    I have added the butterknife to this project  and also an on click listener to show waht will happen as soon as i click the button

--- app/src/main/java/com/example/dennis/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/dennis/weatherapp/MainActivity.java
@@ -2,14 +2,32 @@ package com.example.dennis.weatherapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    //This is the binds. I bind all my components here at the top
+    @Bind(R.id.myEditText) EditText mEditText;
+    @Bind(R.id.myButton) Button mButton;
 
+//This is where the app will run from
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+//IMplement my Butter knife for the binds above
+        ButterKnife.bind(this);
+//This is the action for clicking of the button
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Defines what happens when the button is clicked
+                String location = mEditText.getText().toString();
+            }
+        });
     }
 }
