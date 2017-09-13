89961a824cfee5f9ce38b071667789f5988a709c
dennismuturia <dmuturia2922@gmail.com>
2017-09-13 07:45:49 +0300

    Added a querycall for the api
    Also added a build config in the gradle

---
+++ app/src/main/java/com/example/dennis/weatherapp/Constants.java
@@ -0,0 +1,10 @@
+package com.example.dennis.weatherapp;
+
+/**
+ * Created by dennis on 9/12/17.
+ */
+
+public class Constants {
+    public static final String OPEN_WEATHER = BuildConfig.OPEN_WEATHER;
+    public static final String OPEN_WEATHER_BASE_URL = "api.openweathermap.org/data/2.5/weather?q=location";
+}
